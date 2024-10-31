a565a353d5bc6303d375ac5cffd4710af8cdea92
dongha <rmaehdgk@gamil.com>
2024-10-31 02:06:59 +0000

    10-31 11:07 modify

--- chap5/prob5/main.c
+++ chap5/prob5/main.c
@@ -6,20 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-	int fd;
+	FILE *fd;
 	struct student record;
 	if (argc <2) {
 		fprintf(stderr,"How to use: %s file\n", argv[0]);
 		exit(1);
 	}
-	if((fd = open(argv[1],O_WRONLY|O_CREAT|O_EXCL,0640))==-1){
+	if((fd = fopen(argv[1],"w"))==NULL){
 		perror(argv[1]);
 		exit(2);
 	}
 	printf("%-9s %-8s %-4s\n", "StuID","Name","Score");
 	while (scanf("%d %s %d", &record.id, record.name, &record.score) == 3) {
 		lseek(fd, (record.id-START_ID)*sizeof(record),SEEK_SET);
-		write(fd,(char *)&record,sizeof(record));
+		fprintf(fd, "%d %s %d\n",record.id, record.name , record.score);
 	}
 	close(fd);
 	exit(0);
